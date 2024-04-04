2b5f028f800426eb08e57569fc4cf5de03ba0b8a
Kevin Wu <kevwuhoo15@gmail.com>
2024-04-04 18:18:32 -0400

    updated member dashboard

--- members.py
+++ members.py
@@ -1,6 +1,3 @@
-from config import parseCursorFetch
-import datetime
-
 def registerMember(email, first_name, last_name, conn):
     with conn:
         with conn.cursor() as curs:
@@ -32,21 +29,26 @@ def memberDashboard(email, conn):
                     case "1":
                         curs.execute('SELECT (first_name, last_name, member_email, height, curr_weight) FROM members WHERE member_email = %s', ((email,)))
                         print(curs.fetchone()[0])
+
                     case "2":
                         height = input("Height: ")
                         weight = input("Weight: ")
                         curs.execute('UPDATE members SET height = %s, curr_weight = %s WHERE member_email = %s', ((height, weight, email)))
                         conn.commit()
+
                     case "3":
                         curs.execute('SELECT (goal_weight, goal_desc) FROM members WHERE member_email = %s', ((email,)))
                         print(curs.fetchone()[0])
+
                     case "4":
                         goal_weight = input("Goal Weight: ")
                         goal_desc = input("Goal Description: ")
                         curs.execute('UPDATE members SET goal_weight = %s, goal_desc = %s WHERE member_email = %s', ((goal_weight, goal_desc, email)))
                         conn.commit()
+
                     case "5":
                         print("FITNESS ACHIEVEMENTS")
+
                     case "6":
                         curs.execute('SELECT * FROM training_sessions WHERE member_email = %s', ((email,)))
                         print("Registered Personal Training Sessions")
@@ -55,6 +57,7 @@ def memberDashboard(email, conn):
                             print("Session ID: " + str(row[0]))
                             print("Trainer: " + row[1])
                             print("Time: " + str(row[2]) + "\n")
+
                     case "7":
                         curs.execute('SELECT * FROM training_sessions WHERE member_email IS NULL')
                         rows = curs.fetchall()
@@ -68,11 +71,29 @@ def memberDashboard(email, conn):
                             if curs.fetchall() != []:
                                 curs.execute('UPDATE training_sessions SET member_email = %s WHERE session_id = %s', ((email, choice)))
                                 conn.commit()
-                                print("Successfully registered for the session!\n")
+                                print("Successfully booked this session!\n")
                             else:
                                 print("Session ID invalid")
                         else:
                             print("No available sessions")
+                    
+                    case "8":
+                        curs.execute('SELECT * FROM training_sessions WHERE member_email = %s', ((email,)))
+                        print("Registered Personal Training Sessions")
+                        rows = curs.fetchall()
+                        for row in rows:
+                            print("Session ID: " + str(row[0]))
+                            print("Trainer: " + row[1])
+                            print("Time: " + str(row[2]) + "\n")
+                        choice = input("Cancel which session?: ")
+                        curs.execute('SELECT * FROM training_sessions WHERE session_id = %s AND member_email = %s', ((choice, email)))
+                        if curs.fetchall() != []:
+                            curs.execute('UPDATE training_sessions SET member_email = NULL WHERE session_id = %s', ((choice,)))
+                            conn.commit()
+                            print("Successfully cancelled this session\n")
+                        else:
+                            print("Session ID invalid")
+
                     case "10":
                         break
             curs.close()
